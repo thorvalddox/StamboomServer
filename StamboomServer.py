4d73a2b8e9e6e482c9c1f82db1853a9e65527ce7
thorvalddox <thorvalddx94@gmail.com>
2015-06-11 20:02:19 +0200

    Added interactive family tree

--- StamboomServer.py
+++ StamboomServer.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import render_template,make_response
 
 import core
+import draw
 
 
 
@@ -16,11 +17,35 @@ app.debug = True
 #TODO conversion
 
 
-@app.route('/stamboom/commands')
-def hello_world():
+@app.route('/stamboom/commands/oldxml')
+def show_old_xml():
     response = make_response(render_template("code_show.html",code=core.xmlTest().replace("\n","<br/>")))
     return response
 
+@app.route('/stamboom/commands/handled')
+def show_handled_code():
+    response = make_response(render_template("code_show.html",code=core.bashTest().replace("\n","<br/>")))
+    return response
+
+@app.route('/stamboom/commands/raw')
+def show_raw_code():
+    response = make_response(render_template("code_show.html",code=core.rawCode().replace("\n","<br/>")))
+    return response
+
+
+@app.route('/stamboom/')
+def show_fam_tree():
+    f = core.FamilyTree()
+    f.from_code("data.log")
+    d = draw.drawPeople(f)
+    response = make_response(render_template("famtree.html",canvas=d.get_html_canvas(),script=d.get_html_script()))
+    return response
+
+
+@app.route('/edit/<name>/')
+def edit(name):
+    response = make_response(render_template("edit.html",name=name))
+    return response
 
 if __name__ == '__main__':
     app.run()
