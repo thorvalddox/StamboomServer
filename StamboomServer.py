7ce43140b3e5ae07a04ac5f0eee06f2ac289771c
thorvalddox <thorvalddx94@gmail.com>
2015-06-11 15:31:46 +0200

    Added core mechanincs, loading from the old format and saving to the new format

--- StamboomServer.py
+++ StamboomServer.py
@@ -1,11 +1,25 @@
 from flask import Flask
 
+from flask import render_template,make_response
+
+import core
+
+
+
 app = Flask(__name__)
 
+app.debug = True
 
-@app.route('/')
+#TODO image uploader
+#TODO interactive site
+#TODO command set
+#TODO conversion
+
+
+@app.route('/stamboom/commands')
 def hello_world():
-    return 'Hello World!'
+    response = make_response(render_template("code_show.html",code=core.xmlTest().replace("\n","<br/>")))
+    return response
 
 
 if __name__ == '__main__':
