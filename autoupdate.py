dbb992c073ce53a641d75a2b1a47b99de02bcb9c
thorvalddox <thorvalddx94@gmail.com>
2015-06-18 11:40:36 +0200

    Added restart after update.
    Updating now happens before restarting instead of after.
    This way, the server could still be running during an update.

--- autoupdate.py
+++ autoupdate.py
@@ -15,14 +15,34 @@ def check_for_changes() -> bool:
     print("Checking for changes")
     proc = subprocess.Popen([BASH_CHANGE], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
-    print("git:", out)
+    print("needs update:", out)
     print("Done checking")
+    return out==b'True\n'
 
 
 #Pythonanywhere free account doesn't allow threading. Do not use this function.
-def run_update_process():
-    threading.Timer(5.0, run_update_process).start()
-    update()
+# def run_update_process():
+#     threading.Timer(5.0, run_update_process).start()
+#     update()
 
 def update():
-    check_for_changes()
+    if check_for_changes():
+        force_update()
+        restart_server()
+
+
+def force_update():
+    print("Updating Code")
+    proc = subprocess.Popen(["git pull"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    print("git:", out)
+    print("Done updating Code")
+
+def restart_server():
+    print("Restart Server")
+    proc = subprocess.Popen(["touch /var/www/wsgi.py"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    print("touch:", out)
+
+def get_commit_number():
+    pass
