aa3f1e1c061025722f247c1328b39901419a8ae3
thorvalddox <thorvalddx94@gmail.com>
2016-06-24 12:43:01 +0200

    Fixed ValueError: too many values to unpack

--- StamboomServer.py
+++ StamboomServer.py
@@ -427,7 +427,7 @@ def render_login(path):
 @app.route("/<path:path>/login/invalid/")
 @catch_errors
 def render_login_invalid(path):
-    response = make_response(render_template("login.html", message="The password combination was invalid", path=path))
+    response = make_response(render_template("google-login-button.html", message="The password combination was invalid", path=path))
     return response
 
 
@@ -435,7 +435,7 @@ def render_login_invalid(path):
 @catch_errors
 def render_login_admin(path):
     response = make_response(
-        render_template("login.html", message="You need to be an admin to view this page", path=path))
+        render_template("google-login-button.html", message="You need to be an admin to view this page", path=path))
     return response
 
 
--- loginhandle2.py
+++ loginhandle2.py
@@ -3,7 +3,7 @@ import json
 def check_credentials(session):
     with open("StamboomServer/userlist.json") as file:
         all_names = json.load(file)
-        for k,v in all_names:
+        for k,v in all_names.items():
             if session.get(k,...) in v:
                 return True
     return
